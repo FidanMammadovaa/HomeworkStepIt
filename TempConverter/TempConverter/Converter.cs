b9fb5ad956fafe9829a8c0800c12793c6ef80c6d
FidanMammadovaa <113114096+FidanMammadovaa@users.noreply.github.com>
2022-09-22 16:11:58 +0400

    Converter

---
+++ TempConverter/TempConverter/Converter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Converter
+{
+    public string? ValueToConvert { get; set; }
+    public double amount { get; set; }
+}
---
+++ TempConverter/TempConverter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Text.Json;
+
+List<Converter> my_converters = new()
+{
+    new(){ValueToConvert = "AZN", amount = 5},
+    new(){ValueToConvert = "RUB"}
+};
+
+WebClient client = new();
+
+var url = $"https://api.apilayer.com/exchangerates_data/convert?to=";
+
+client.Headers.Add("apikey", "o7m41tMIwyiRSkLpyhwcMF9yUaSfz1wQ");
+
+string link = $"{url}{my_converters[1].ValueToConvert}&from={my_converters[0].ValueToConvert}&amount={my_converters[0].amount}";
+
+
+var res = client.DownloadString(link);
+Console.WriteLine(res);
+
+Serialize(res);
+
+void Serialize(string result)
+{
+    using var file = new FileStream("data.json", FileMode.OpenOrCreate);
+    JsonSerializer.Serialize(file, result);
+}
